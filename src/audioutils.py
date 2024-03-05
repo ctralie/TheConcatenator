Fixing bug with DC normalization

src/audioutils.py
@@ -23,7 +23,14 @@ def blackman_harris_window(N):
     t = np.arange(N)/N
     return a0 - a1*np.cos(2*np.pi*t) + a2*np.cos(4*np.pi*t) - a3*np.cos(6*np.pi*t)
 
-def get_windowed(x, hop, win, win_fn=hann_window):
+def tri_window(N):
+    h = N//2
+    ret = np.zeros(N)
+    ret[0:h] = np.linspace(0, 1, h)
+    ret[h:2*h] = np.linspace(1, 0, h)
+    return ret
+
+def get_windowed(x, hop, win, win_fn=hann_window, dc_normalize=True):
     """
     Stack sliding windows of audio, multiplied by a window function,
     into the columns of a 2D array
@@ -36,10 +43,12 @@ def get_windowed(x, hop, win, win_fn=hann_window):
         Hop length
     win_fn: int -> ndarray(win)
         Window function
+    dc_normalize: bool
+        If True, normalize the windows to sum to 0 after windowing
 
     Returns
     -------
-    torch.tensor(win, n_windows)
+    ndarray(win, n_windows)
         Windowed audio
     """
     nwin = int(np.ceil((x.size-win)/hop))+1
@@ -47,7 +56,10 @@ def get_windowed(x, hop, win, win_fn=hann_window):
     for j in range(nwin):
         xj = x[hop*j:hop*j+win]
         S[0:xj.size, j] = xj
-    return S*win_fn(win)[:, None]
+    S = S*win_fn(win)[:, None]
+    if dc_normalize:
+        S -= np.mean(S, axis=0, keepdims=True)
+    return S
 
 def do_windowed_sum(WSound, H, win, hop):
     """
@@ -104,7 +116,6 @@ def load_corpus(path, sr, stereo):
             x, sr = librosa.load(f, sr=sr, mono=not stereo)
             if stereo and len(x.shape) == 1:
                 x = np.array([x, x])
-            x -= np.mean(x, axis=1, keepdims=True)
             samples.append(x)
         except:
             pass

src/particle.py
@@ -5,7 +5,7 @@ from sklearn.neighbors import KDTree
 from probutils import stochastic_universal_sample, do_KL_torch, count_top_activations, get_activations_diff, get_repeated_activation_itervals, get_diag_lengths
 from observer import Observer
 from propagator import Propagator
-from audioutils import get_windowed, hann_window
+from audioutils import get_windowed, hann_window, tri_window
 from audiofeatures import AudioFeatureComputer
 import pyaudio
 import struct
@@ -87,7 +87,6 @@ class ParticleFilter:
         tic = time.time()
         win = feature_params["win"]
         sr = feature_params["sr"]
-        self.win_samples = np.array(hann_window(win), dtype=np.float32)
         self.p = particle_params["p"]
         self.P = particle_params["P"]
         self.pfinal = particle_params["pfinal"]
@@ -111,6 +110,8 @@ class ParticleFilter:
         n_channels = ycorpus.shape[0]
         self.n_channels = n_channels
         self.WSound = [get_windowed(ycorpus[i, :], hop, win, hann_window) for i in range(n_channels)]
+        # Corpus is analyzed with hann window
+        self.win_samples = np.array(hann_window(win), dtype=np.float32)
         WCorpus = torch.concatenate(tuple([self.feature_computer(W) for W in self.WSound]), axis=0)
         self.WCorpus = WCorpus
 
@@ -442,10 +443,14 @@ class ParticleFilter:
         repeated_intervals = get_repeated_activation_itervals(H, p)
         
         plt.subplot(231)
-        plt.plot(active_diffs)
+        active_diffs = np.cumsum(active_diffs)
+        avgwin = 10
+        active_diffs = (active_diffs[avgwin::] - active_diffs[0:-avgwin])/avgwin
+        t = np.arange(active_diffs.size)*self.win/(self.sr*2)
+        plt.plot(t, active_diffs)
         plt.legend(["Particle Filter: Mean {:.3f}".format(np.mean(active_diffs))])
         plt.title("Activation Changes over Time, p={}, proposal_k={}".format(self.p, self.proposal_k))
-        plt.xlabel("Timestep")
+        plt.xlabel("Time (Seconds)")
 
         plt.subplot(232)
         plt.hist(active_diffs, bins=np.arange(30))