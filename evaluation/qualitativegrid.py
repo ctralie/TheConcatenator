3ea44c725a4bf9730ae00b27888b957b9a816f54
Chris Tralie <chris.tralie@gmail.com>
2024-04-03 23:48:42 -0400

    Beginning to run Ben's qualitative examples

---
+++ evaluation/qualitativegrid.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+import sys
+import os
+import time
+import subprocess
+sys.path.append("../src")
+from particle import *
+from probutils import *
+from audioutils import *
+
+
+## Step 1: intialize parameters
+sr = 44100
+win = 2048
+hop = win//2
+stereo = True
+P = 1000
+feature_params = dict(
+    win=win,
+    sr=sr,
+    min_freq=0,
+    max_freq=8000,
+    use_stft=True,
+    use_mel=False,
+    mel_bands=40,
+    use_zcs=False,
+)
+particle_params = dict(
+    p=10,
+    pfinal=10,
+    pd=0.95,
+    temperature=10,
+    L=10,
+    P=P,
+    r=3,
+    neff_thresh=0.1*P,
+    proposal_k=0,
+    alpha=0.1,
+    use_top_particle=False
+)
+
+targets = [
+            "2 Voice Harmony/Let It Be - 2 voice cp - filt saw - 2 octaves.wav",
+            "2 Voice Harmony/Let It Be - 2 voice cp - sine - 2 octaves.wav",
+            "3 Voice Harmony/3 voice cp - sine - 2 oct, 1st.wav",
+            "3 Voice Harmony/3 voice cp - synth filt - 2 oct, 1st.wav",
+            "Melody/Let It Be - Melody - filt saw - 2 octaves.wav",
+            "Melody/Let It Be - Melody - sine - 2 octaves.wav",
+            "Ripple Continuum/Ripple Contiuum - filt saw - 3 octaves.wav",
+            "Ripple Continuum/Ripple Contiuum Piano Solo.wav",
+            "Ripple Continuum/Ripple Contiuum - sine - 3 octaves.wav",
+            "Beatles_LetItBe.mp3"
+           ]
+
+corpora = [
+    "Bees_Buzzing.mp3",
+    "Beethoven - Symphony No. 5 in C Minor, Op. 67_ I. Allegro con brio.m4a",
+    "Bjork It's Oh So Quiet.m4a",
+    "Skrillex - Scary Monsters and Nice Sprites.m4a",
+    "Foley FX",
+    "Tonal",
+    "Vocals",
+    "Percussive",
+    "Pink Floyd - The Wall",
+    "Skrillex - Quest For Fire",
+    "Mr. Bill - Spectra Sample Pack Excerpt"
+]
+
+## Step 2: Initialize corpus
+html = "<html>\n<body>\n<table>\n<tr><td></td>"
+for target in targets:
+    html += "<td><h3>{}</h3></td>".format(target)
+html += "</tr>"
+for corpus in corpora:
+    corpus_name = corpus.split("/")[-1]
+    ycorpus = load_corpus("Qualitative/Corpus/" + corpus, sr, stereo=True)
+    pf = ParticleFilter(ycorpus, feature_params, particle_params, 'cuda')
+    html += "<tr><td><h3>{}</h3></td>".format(corpus)
+    for target in targets:
+        target_name = target.split("/")[-1]
+        print(corpus, target)
+        path = "{}_{}.wav".format(corpus_name, target_name)
+        pathmp3 = "{}_{}.mp3".format(corpus_name, target_name)
+        if not os.path.exists("Qualitative/Results/" + pathmp3):
+            pf.reset_state()
+            ytarget = load_corpus("Qualitative/Targets/"+target, sr=sr, stereo=stereo)
+            tic = time.time()
+            pf.process_audio_offline(ytarget)
+            generated = pf.get_generated_audio()
+            wavfile.write(path, sr, generated)
+            if os.path.exists("Qualitative/Results/"+pathmp3):
+                os.remove("Qualitative/Results/"+pathmp3)
+            cmd = ["ffmpeg", "-i", path, "Qualitative/Results/"+pathmp3]
+            print(cmd)
+            subprocess.call(cmd)
+            os.remove(path)
+        html += "<td><audio controls><source src=\"{}\" type=\"audio/mp3\"></audio></td>".format(pathmp3)
+        fout = open("Qualitative/Results/index.html", "w")
+        fout.write(html)
+        fout.close()
+    html += "</tr>\n"
+
